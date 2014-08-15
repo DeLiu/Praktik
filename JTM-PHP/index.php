2461a16248d5f92b47024034c18404dcb715d9f4
Patrick Larsen <hatsen2000@gmail.com>
2014-08-15 10:42:32 +0200

    Added more files for the PHP site.

--- JTM-PHP/index.php
+++ JTM-PHP/index.php
@@ -1,5 +1,9 @@
+<!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="./css/style.css">
+		<link rel="icon" type="image/ico" href="./images/favicon.ico">
 		<title>JTM-Landbrug</title>
 	</head>
 	<body>
