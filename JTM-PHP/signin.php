6cccb7e5d04df3a63f3c15222da15bf6c54b7577
Patrick Larsen <hatsen2000@gmail.com>
2014-08-20 11:47:45 +0200

    Oversættelse af log ind-siden.

--- JTM-PHP/signin.php
+++ JTM-PHP/signin.php
@@ -7,7 +7,7 @@ echo '<h3>Sign in</h3>';
 //first, check if the user is already signed in. If that is the case, there is no need to display this page
 if(isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true)
 {
-    echo 'You are already signed in, you can <a href="signout.php">sign out</a> if you want.';
+    echo 'Du er allerede logget ind, du kan logge <a href="signout.php">ud</a>, hvis du ønsker.';
 }
 else
 {
@@ -16,9 +16,9 @@ else
         /*the form hasn't been posted yet, display it
           note that the action="" will cause the form to post to the same page it is on */
         echo '<form method="post" action="">
-            Username: <input type="text" name="user_name" />
-            Password: <input type="password" name="user_pass">
-            <input type="submit" value="Sign in" />
+            Brugernavn: <input type="text" name="user_name" />
+            Adgangskode: <input type="password" name="user_pass">
+            <input type="submit" value="Log ind" />
          </form>';
     }
     else
@@ -32,12 +32,12 @@ else
          
         if(!isset($_POST['user_name']))
         {
-            $errors[] = 'The username field must not be empty.';
+            $errors[] = 'Indtast venligst et brugernavn.';
         }
          
         if(!isset($_POST['user_pass']))
         {
-            $errors[] = 'The password field must not be empty.';
+            $errors[] = 'Indtast venligst en adgangskode.';
         }
          
         if(!empty($errors)) /*check for an empty array, if there are errors, they're in this array (note the ! operator)*/
@@ -80,7 +80,7 @@ else
                 //2. the query returned an empty result set, the credentials were wrong
                 if(mysql_num_rows($result) == 0)
                 {
-                    echo 'You have supplied a wrong user/password combination. Please try again.';
+                    echo 'Brugernavnet og adgangskoden passer ikke sammen. Prøv igen.';
                 }
                 else
                 {
@@ -95,7 +95,7 @@ else
                         $_SESSION['user_level'] = $row['user_level'];
                     }
                      
-                    echo 'Welcome, ' . $_SESSION['user_name'] . '. <a href="index.php">Proceed to the forum overview</a>.';
+                    echo 'Welcome, ' . $_SESSION['user_name'] . '. <a href="index.php">Fortsæt til forummet</a>.';
                 }
             }
         }
