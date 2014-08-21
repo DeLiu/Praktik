035bc39d8b7c7c1a1034bdf6a1655df829055127
DeLiu <de.liu@ofir.dk>
2014-08-21 09:57:49 +0200

    Added CMS masterpage, Edit Link & Products

---
+++ JTM-PHP/create_topic.php
@@ -0,0 +1,140 @@
+<?php
+include 'connect.php';
+include 'header.php';
+ 
+echo '<h2>Opret en tråd</h2>';
+if (isset($_SESSION['signed_in']) == false)
+{
+    //the user is not signed in
+    echo 'Du skal være <a href="/forum/signin.php">logget ind</a>, før du kan oprette en tråd.';
+}
+else
+{
+    //the user is signed in
+    if($_SERVER['REQUEST_METHOD'] != 'POST')
+    {   
+        //the form hasn't been posted yet, display it
+        //retrieve the categories from the database for use in the dropdown
+        $sql = "SELECT
+                    cat_id,
+                    cat_name,
+                    cat_description
+                FROM
+                    categories";
+         
+        $result = mysql_query($sql);
+         
+        if(!$result)
+        {
+            //the query failed, uh-oh :-(
+            echo 'Error while selecting from database. Please try again later.';
+        }
+        else
+        {
+            if(mysql_num_rows($result) == 0)
+            {
+                //there are no categories, so a topic can't be posted
+                if($_SESSION['user_level'] == 1)
+                {
+                    echo 'Du har endnu ikke oprettet nogle subforummer.';
+                }
+                else
+                {
+                    echo 'Før du kan oprette en tråd, skal du vente på at der oprettes subforummer.';
+                }
+            }
+            else
+            {
+         
+                echo '<form method="post" action="">
+                    Emne: <input type="text" name="topic_subject" />
+                    Subforum:'; 
+                 
+                echo '<select name="topic_cat">';
+                    while($row = mysql_fetch_assoc($result))
+                    {
+                        echo '<option value="' . $row['cat_id'] . '">' . $row['cat_name'] . '</option>';
+                    }
+                echo '</select>'; 
+                     
+                echo 'Besked: <textarea name="post_content" /></textarea>
+                    <input type="submit" value="Opret tråd" />
+                 </form>';
+            }
+        }
+    }
+    else
+    {
+        //start the transaction
+        $query  = "BEGIN WORK;";
+        $result = mysql_query($query);
+         
+        if(!$result)
+        {
+            //Damn! the query failed, quit
+            echo 'An error occured while creating your topic. Please try again later.';
+        }
+        else
+        {
+     
+            //the form has been posted, so save it
+            //insert the topic into the topics table first, then we'll save the post into the posts table
+            $sql = "INSERT INTO 
+                        topics(topic_subject,
+                               topic_date,
+                               topic_cat,
+                               topic_by)
+                   VALUES('" . mysql_real_escape_string($_POST['topic_subject']) . "',
+                               NOW(),
+                               " . mysql_real_escape_string($_POST['topic_cat']) . ",
+                               " . $_SESSION['user_id'] . "
+                               )";
+                      
+            $result = mysql_query($sql);
+            if(!$result)
+            {
+                //something went wrong, display the error
+                echo 'An error occured while inserting your data. Please try again later.' . mysql_error();
+                $sql = "ROLLBACK;";
+                $result = mysql_query($sql);
+            }
+            else
+            {
+                //the first query worked, now start the second, posts query
+                //retrieve the id of the freshly created topic for usage in the posts query
+                $topicid = mysql_insert_id();
+                 
+                $sql = "INSERT INTO
+                            posts(post_content,
+                                  post_date,
+                                  post_topic,
+                                  post_by)
+                        VALUES
+                            ('" . mysql_real_escape_string($_POST['post_content']) . "',
+                                  NOW(),
+                                  " . $topicid . ",
+                                  " . $_SESSION['user_id'] . "
+                            )";
+                $result = mysql_query($sql);
+                 
+                if(!$result)
+                {
+                    //something went wrong, display the error
+                    echo 'An error occured while inserting your post. Please try again later.' . mysql_error();
+                    $sql = "ROLLBACK;";
+                    $result = mysql_query($sql);
+                }
+                else
+                {
+                    $sql = "COMMIT;";
+                    $result = mysql_query($sql);
+                     
+                    //after a lot of work, the query succeeded!
+                    echo 'Tillykke, her er din nyoprettede <a href="topic.php?id='. $topicid . '">tråd</a>.';
+                }
+            }
+        }
+    }
+}
+include 'footer.php';
+?>
---
+++ JTM-PHP/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="nl" lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Et forum omkring økologi og landbrug" />
+    <meta name="keywords" content="agriculture, økologi, landbrug" />
+    <title>JTM-Landbrug forum</title>
+    <link rel="stylesheet" href="./css/style.css" type="text/css">
+</head>
+<body>
+<h1>JTM-Landbrug - Forum</h1>
+    <div id="wrapper">
+    <div id="menu">
+        <a class="item" href="index.php">Hjem</a> -
+        <a class="item" href="create_topic.php">Opret en tråd</a> -
+        <a class="item" href="create_cat.php">Opret et subforum</a>
+         
+        <div id="userbar">
+        <?php
+			echo '<div id="userbar">';
+				if(isset($_SESSION['signed_in']))
+				{
+					echo 'Velkommen' . $_SESSION['user_name'] . '. Ikke dig? <a href="signout.php">Log ud</a>';
+				}
+				else
+				{
+					echo '<a href="signin.php">Log ind</a> or <a href="signup.php">Opret en bruger</a>.';
+				}
+			echo '</div>';
+?>
+    </div>
+        <div id="content">
