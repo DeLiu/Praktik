76c221e4bf20649458054756053a6ffd3f1cd2f6
Patrick Larsen <hatsen2000@gmail.com>
2014-09-04 12:23:45 +0200

    En start på et reply-system

---
+++ JTM/Forum/Delete_Topic.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Forum/ForumSiteMaster.master" AutoEventWireup="true" CodeFile="Delete_Topic.aspx.cs" Inherits="Forum_Delete_Topic" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="content" Runat="Server">
+    <div id="content" runat="server">
+
+    </div>
+</asp:Content>
+
---
+++ JTM/Forum/Reply.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Forum_Reply : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SQLDatabase db = new SQLDatabase("ForumDB.mdf", "LocalDB", "", "");
+        string html = "";
+
+        try
+        {
+            db.Open();
+            db.Exec("INSERT INTO posts(post_content, post_date, post_by) VALUES ('" + Request.Form["reply-content"] + "', NOW(), " + Request.QueryString["id"] + ", " + "0");
+
+        }
+        catch (Exception ex)
+        {
+            html = "Dit indlæg kunne ikke gemmes. Prøv igen senere.";
+        }
+        finally
+        {
+            html += "Dit indlæg er gemt, se det <a href='Topic.aspx?id=" + Request.QueryString["id"] + "'>her</a>.";
+            content.InnerHtml = html;
+            db.Close();
+        }
+    }
+}
