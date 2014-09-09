e2c65cb11dac3797b0551fadc834350e03891379
Patrick Larsen <hatsen2000@gmail.com>
2014-09-09 10:45:54 +0200

    Man kan nu låse tråde

---
+++ JTM/Forum/Lock_Topic.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Forum/ForumSiteMaster.master" AutoEventWireup="true" CodeFile="Lock_Topic.aspx.cs" Inherits="Forum_Lock_Topic" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="content" Runat="Server">
+    <div id="content" runat="server">
+
+    </div>
+</asp:Content>
+
---
+++ JTM/Forum/Lock_Topic.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Forum_Lock_Topic : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SQLDatabase db = new SQLDatabase("ForumDB.mdf", "LocalDB", "", "");
+        string html = "";
+
+        if (2 + 4 == 1)
+        {
+            try
+            {
+                db.Open();
+                db.Exec("UPDATE topics SET topic_locked = 1, WHERE topic_id =" + Request.QueryString["id"]);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                db.Close();
+                html = "Tråden er nu låst, du kan vende tilbage til tråden <a href='Topic.aspx?id=" + Request.QueryString["id"] + "'>her</a>.";
+                content.InnerHtml = html;
+            }
+        }
+        else
+        {
+            content.InnerHtml = "Du har vist ikke noget at gøre her. Vend tilbage til forsiden ved at klikke <a href='Default.aspx'>på dette link</a>.";
+        }
+    }
+}
