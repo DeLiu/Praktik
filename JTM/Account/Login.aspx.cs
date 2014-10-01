10d94472ef15b7f358ca5da4d928b854c38dff53
DeLiu <de.liu@ofir.dk>
2014-10-01 10:02:02 +0200

    Added Login in Webpage

--- JTM/Account/Login.aspx.cs
+++ JTM/Account/Login.aspx.cs
@@ -4,12 +4,13 @@ using System;
 using System.Web;
 using System.Web.UI;
 using JTM;
+using crypto;
 
 public partial class Account_Login : Page
 {
         protected void Page_Load(object sender, EventArgs e)
         {
-            /*
+            
             RegisterHyperLink.NavigateUrl = "Register";
             OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
             var returnUrl = HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
@@ -17,11 +18,44 @@ public partial class Account_Login : Page
             {
                 RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
             }
-             */
+             
         }
 
         protected void LogIn(object sender, EventArgs e)
         {
+            if (Page.IsValid)
+            {
+                SQLDatabase DB = new SQLDatabase("ForumDB.mdf", "LocalDB", "", "");
+
+                try
+                {
+                    DB.Open();
+                    string[][] userlog = DB.Query("SELECT user_name, user_pass, userlevel FROM Users WHERE user_name='" + UserName.Text + "'");
+
+                    if (PasswordHash.ValidatePassword(Password.Text, userlog[0][1]) == true)
+                    {
+                        Session.Add("User", userlog[0][0]);
+                        Session.Add("Pass", userlog[0][1]);
+                        Session.Add("level", userlog[0][2]);
+                        Session.Timeout = 5;
+                        //IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+                        //Response.Redirect(Request.UrlReferrer.ToString());
+                    }
+                    else
+                    {
+                        FailureText.Text = "Forkert kode eller brugernavn prøv igen.";
+                        ErrorMessage.Visible = true;
+                    }
+                }
+                catch (Exception ex)
+                { }
+                finally
+                {
+                    DB.Close();
+                }
+            }
+
+            /*
             if (IsValid)
             {
                 // Validate the user password
@@ -37,6 +71,6 @@ public partial class Account_Login : Page
                     FailureText.Text = "Invalid username or password.";
                     ErrorMessage.Visible = true;
                 }
-            }
+            } */
         }
 }
--- JTM/AdminRequiredContent/CMS.master.cs
+++ JTM/AdminRequiredContent/CMS.master.cs
@@ -7,6 +7,18 @@ using System.Web.UI.WebControls;
 
 public partial class AdminRequiredContent_CMS : System.Web.UI.MasterPage
 {
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        if (Session["level"] != null)
+        {
+            if ((string)Session["level"] != "0")
+            {
+                Response.Redirect("~/Account/Login.aspx");
+            }
+        }
+        else
+            Response.Redirect("~/Account/Login.aspx");
+    }
     protected void Page_Load(object sender, EventArgs e)
     {
         Statistik sk = new Statistik();
@@ -15,6 +27,11 @@ public partial class AdminRequiredContent_CMS : System.Web.UI.MasterPage
 
     protected void Log_out(object sender, LoginCancelEventArgs e)
     {
-        Context.GetOwinContext().Authentication.SignOut();
+        
+        //Context.GetOwinContext().Authentication.SignOut();
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Session.Abandon();
     }
 }
